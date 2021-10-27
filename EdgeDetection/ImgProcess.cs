93eeb10c2bc5373554b33ef96cb2da0e426c2f33
MaciekLPL <maciekl2299@gmail.com>
2021-10-27 18:45:52 +0200

    Add project files.

---
+++ EdgeDetection/ImgProcess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdgeDetection {
+    class ImgProcess {
+        private static double[,] KernelX => new double[,]
+                    {
+                        {-1, 0, 1},
+                        {-2, 0, 2},
+                        {-1, 0, 1}
+                    };
+
+        private static double[,] KernelY => new double[,]
+                    {
+                        { 1, 2, 1},
+                        { 0, 0, 0},
+                        {-1,-2,-1}
+                    };
+
+        public void grayscale() {
+
+
+
+        }
+
+    }
+}
---
+++ EdgeDetection/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EdgeDetection {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void btnSelectImage_Click(object sender, RoutedEventArgs e) {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.DefaultExt = ".png";
+            dialog.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+            Nullable<bool> result = dialog.ShowDialog();
+
+            if (result == true) {
+                string filename = dialog.FileName;
+                textblockTimer.Text = filename;
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap) {
+
+            using (MemoryStream memory = new MemoryStream()) {
+                bitmap.Save(memory, ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+                return bitmapimage;
+
+            }
+        }
+
+    }
+}
