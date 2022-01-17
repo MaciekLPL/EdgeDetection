39257df8ad71cc3bb6ff107166ceeeadc2f6b63e
MaciekLPL <maciekl2299@gmail.com>
2022-01-17 19:31:48 +0100

    added protection against overwriting the file and against too small input image

--- EdgeDetection/ImgProcessing.cs
+++ EdgeDetection/ImgProcessing.cs
@@ -39,39 +39,39 @@ namespace EdgeDetection {
 
         private static void CSharp(byte* ptrOriginal, byte* ptrResult, int width, int height, int stride, int threads) {
 
-            sbyte[,] kX = new sbyte[,] { { 1, 0, -1 }, { 2, 0, -2 }, { 1, 0, -1 } };
+            sbyte[,] kX = new sbyte[,] { { 1, 0, -1 }, { 2, 0, -2 }, { 1, 0, -1 } };                                    //Kernels definitions
             sbyte[,] kY = new sbyte[,] { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
 
-            _ = Parallel.For(1, height - 1, new ParallelOptions { MaxDegreeOfParallelism = threads }, y => {
-                for (int x = 1; x < width - 1; x++) {
+            _ = Parallel.For(1, height - 1, new ParallelOptions { MaxDegreeOfParallelism = threads }, y => {            //Row loop (parallel)
+                for (int x = 1; x < width - 1; x++) {                                                                   //Column loop
 
-                    int centerPixel = (y * stride) + (x * 4);
-                    int rx = 0, ry = 0, gx = 0, gy = 0, bx = 0, by = 0;
+                    int centerPixel = (y * stride) + (x * 4);                                                           //Central pixel (in matrix)
+                    int rx = 0, ry = 0, gx = 0, gy = 0, bx = 0, by = 0;                                                 //Thread safe variables initialization
 
-                    for (int matY = 0; matY < 3; matY++) {
-                        for (int matX = 0; matX < 3; matX++) {
+                    for (int matY = 0; matY < 3; matY++) {                                                              //Matrix loop (Y)
+                        for (int matX = 0; matX < 3; matX++) {                                                          //Matrix loop (X)
 
-                            byte* currentPixel = ptrOriginal + ((y + matY - 1) * stride) + ((x + matX - 1) * 4);
-                            sbyte vx = kX[matY, matX];
+                            byte* currentPixel = ptrOriginal + ((y + matY - 1) * stride) + ((x + matX - 1) * 4);        //Pixel currently being computed 
+                            sbyte vx = kX[matY, matX];                                                                  //Get values from kernels
                             sbyte vy = kY[matY, matX];
 
-                            bx += vx * *currentPixel;
+                            bx += vx * *currentPixel;                                                                   //Multiplying and adding to result (X kernel)
                             gx += vx * *(currentPixel + 1);
                             rx += vx * *(currentPixel + 2);
 
-                            by += vy * *currentPixel;
+                            by += vy * *currentPixel;                                                                   //Multiplying and adding to result (Y kernel)
                             gy += vy * *(currentPixel + 1);
                             ry += vy * *(currentPixel + 2);
 
                         }
                     }
 
-                    double magB = Math.Sqrt((bx * bx) + (by * by));
+                    double magB = Math.Sqrt((bx * bx) + (by * by));                                                     //Gradient magnitude
                     double magG = Math.Sqrt((gx * gx) + (gy * gy));
                     double magR = Math.Sqrt((rx * rx) + (ry * ry));
 
-                    ptrResult[centerPixel] = magB > 255 ? (byte)255 : (byte)magB;
-                    ptrResult[centerPixel + 1] = magG > 255 ? (byte)255 : (byte)magG;
+                    ptrResult[centerPixel] = magB > 255 ? (byte)255 : (byte)magB;                                       //Enter values into output array
+                    ptrResult[centerPixel + 1] = magG > 255 ? (byte)255 : (byte)magG;                                   //255 or gradient magnitude
                     ptrResult[centerPixel + 2] = magR > 255 ? (byte)255 : (byte)magR;
                 }
             });
--- EdgeDetection/MainWindow.xaml.cs
+++ EdgeDetection/MainWindow.xaml.cs
@@ -22,25 +22,26 @@ namespace EdgeDetection {
         private Bitmap inputBitmap;
         private Bitmap resultBitmap;
         private int threads;
+        private string filename;
 
         private void btnSelectImage_Click(object sender, RoutedEventArgs e) {
+
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.DefaultExt = ".jpeg";
             ofd.Filter = "JPEG Image|*.jpg;*.jpeg|Bitmap Image|*.bmp";
 
             if (ofd.ShowDialog() == true) {
-                string filename = ofd.FileName;
+
+                filename = ofd.FileName;
                 inputBitmap = new Bitmap(filename);
                 imgSelected.Source = ImgProcessing.BitmapToImage(inputBitmap);
                 imgFilter.Source = new BitmapImage(new Uri(@"\Resources\no-image.png", UriKind.Relative));
-
-
             }
         }
 
         private void BtnStart_Click(object sender, RoutedEventArgs e) {
 
-            if (inputBitmap != null) {
+            if (inputBitmap != null && inputBitmap.Width > 2 && inputBitmap.Height > 2) {
 
                 timer.Restart();
                 resultBitmap = ImgProcessing.EdgeDetection(inputBitmap, (int)sliderThreads.Value, (bool) radioCS.IsChecked);
@@ -49,18 +50,26 @@ namespace EdgeDetection {
                 imgFilter.Source = ImgProcessing.BitmapToImage(resultBitmap);
                 textblockTimer.Visibility = Visibility.Visible;
                 textblockTimer.Text = $"Timer: {timer.ElapsedMilliseconds}ms";
+
+            } else {
+                MessageBox.Show("The image could not be processed. Check that it has minimum dimensions of 3x3", "Edge Detection - Image error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
         private void BtnSave_Click(object sender, RoutedEventArgs e) {
+
             if (resultBitmap != null) {
 
                 SaveFileDialog sfd = new SaveFileDialog();
                 sfd.DefaultExt = ".jpeg";
-                sfd.Filter = "JPEG Image (*.jpg,*.jpeg)|*.jpg;*.jpeg|Bitmap Image (.bmp)|*.bmp";
+                sfd.Filter = "JPEG Image|*.jpg;*.jpeg|Bitmap Image|*.bmp";
 
                 if (sfd.ShowDialog() == true) {
-                    resultBitmap.Save(sfd.FileName, ImageFormat.Jpeg);
+
+                    if(sfd.FileName != filename)
+                        resultBitmap.Save(sfd.FileName, ImageFormat.Jpeg);
+                    else
+                        MessageBox.Show("You cannot overwrite file you are using!\nTry saving file with different name.", "Edge Detection - Save error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
         }
